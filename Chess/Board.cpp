6abadc0d93ac925748a0c022c6d2cb31b9ec0c99
ChristofferNiska@gmail.com <ChristofferNiska@gmail.com@e0c7d62e-fefe-e873-c81a-245be9499286>
2012-01-20 07:59:40 +0000

    Added the initial project files.

---
+++ Chess/Board.cpp
@@ -0,0 +1,12 @@
+#include "StdAfx.h"
+#include "Board.h"
+
+
+Board::Board(void)
+{
+}
+
+
+Board::~Board(void)
+{
+}
---
+++ Chess/Piece.cpp
@@ -0,0 +1,12 @@
+#include "StdAfx.h"
+#include "Piece.h"
+
+
+Piece::Piece(void)
+{
+}
+
+
+Piece::~Piece(void)
+{
+}
