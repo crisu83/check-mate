4b0a6bc6886e10ea605e72afb4d84cb0e1d1f0d4
ChristofferNiska@gmail.com <ChristofferNiska@gmail.com@e0c7d62e-fefe-e873-c81a-245be9499286>
2012-01-21 20:39:34 +0000

    Continued on the Position and Move classes.

--- Chess/Chess.cpp
+++ Chess/Chess.cpp
@@ -5,7 +5,45 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	char *mv = new char[10];;
+	int turn = 0;
 	Position *position = new Position();
+	Move *move = NULL;
+
+	while (true)
+	{
+		// Empty the screen.
+		system("cls");
+
+		if (move != NULL)
+		{
+			position->execMove(move);
+		}
+
+		// Render the board.
+		position->print();
+
+		std::cout << std::endl;
+			
+		if (move != NULL)
+		{
+			move->print();
+		}
+		
+		std::cout << std::endl << "Next move: ";
+		std::cin >> mv;
+
+		// Wait for a keypress.
+		getchar();
+
+		// Process the move.
+		move = new Move();
+		move->strToMove(mv);
+
+		turn++;
+
+		delete mv;
+	}
 
 	return 0;
 }
